a741008d90cdc7433b063f280bc0daf856952101
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-02-01 14:13:42 +0530

    Create SUMN.C

---
+++ SUMN.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int i,a[10],n,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+}
